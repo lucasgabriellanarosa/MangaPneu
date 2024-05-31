0ef0216fe1a52475b4c1b73290a6f3378089edda
Lucas Gabriel de Lana Rosa <lucas.gabriel.lr2112@gmail.com>
2024-05-31 16:46:42 -0300

    working on chapterFeed.jsx

--- src/pages/ChapterFeed/ChapterFeed.jsx
+++ src/pages/ChapterFeed/ChapterFeed.jsx
@@ -2,13 +2,51 @@ import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom'
-
+import axios from "axios";
 
 
 const ChapterFeed = () => {
+    // const [chapterPages, setChapterPages] = useState([])
+    // let {chapterID} = useParams()
+
+    // useEffect(() => {
+    //     const fetchChapterPageURLs = async () => {
+    //         const response = await axios.get(`https://corsproxy.io/?https://api.mangadex.dev/at-home/server/${chapterID}`);
+
+    //         const {
+    //             baseURL,
+    //             chapter: {hash, data}
+    //         } = response.data
+            
+    //         const pages = data;
+
+    //         const pageURLs = pages.map((page) => `${baseURL}/data/${hash}/${page}`)
+    //         return pageURLs
+    //     }
+
+    //     const fetchPages = async () => {
+    //         const pageURLs = await fetchChapterPageURLs();
+    //         setChapterPages(pageURLs);
+    //     }
+
+        
+
+    // }, [])
+
+
+    // return (
+    //     <div>
+    //         <h1>Helo</h1>
+    //         {chapterPages.map((chapterPage, index) => {
+    //                 <h1>hello</h1>
+    //                 {/* <img key={index} src={chapterPage} /> */}
+    //         })}
+    //     </div>
+    // )
 
     const [mangaData, setMangaData] = useState({})
     const [mangaHash, setMangaHash] = useState()
+    const [mangaBaseURL, setMangaBaseURL] = useState()
 
     let {chapterID} = useParams()
 
@@ -20,14 +58,19 @@ const ChapterFeed = () => {
         .then(data => {
             setMangaData(data.chapter.data)
             setMangaHash(data.chapter.hash)
+            setMangaBaseURL(data.baseUrl)
     })
-    },[])
+    },[])   
+
+console.log(mangaData)
+console.log(mangaHash)
+console.log(mangaBaseURL)
 
 
     return (
         <div>
-            {Object.values(mangaData).map((feedIMG, index) => (
-                <img key={index} src={baseURL + feedIMG} />
+            {Object.values(mangaData).map((feedIMG, index) => (                
+                <img key={index} src={mangaBaseURL + `/data/` + mangaHash + `/` + feedIMG} />
             ))}
         </div>
     )
